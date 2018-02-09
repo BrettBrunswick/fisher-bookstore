68535fd84b92ee049b449c96d5bfd88ca0345076
Brett Brunswick <BrettBrunswick@bretts-mbp-2.home>
2018-02-09 00:52:15 -0500

    Updated view

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Fisher.Bookstore.Models;
 
 namespace Fisher.Bookstore.Controllers
 {
@@ -13,12 +14,32 @@ namespace Fisher.Bookstore.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            var books = GetBooks();
+            return View(books);
         }
 
         public IActionResult New()
         {
             return Content ("Books controller’s New action");
+        }
+
+        private IEnumerable<Book> GetBooks()
+        {
+            var books = new List<Book>();
+
+            books.Add(new Book() {
+                Title = "My First Book",
+                Author = new Author(){Name = "Brett Brunswick"}});
+
+            books.Add(new Book() {
+                Title = "Mr. Misunderstood",
+                Author = new Author(){Name = "Eric Church"}});
+
+            books.Add(new Book() {
+                Title = "Nothing Was the Same",
+                Author = new Author(){Name = "Drake"}});
+
+                return books;        
         }
 
     }
